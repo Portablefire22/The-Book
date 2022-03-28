af79ff8caa84d94c993d7e0156d3c016e4f83610
Portablefire22 <barnowlsarecool@gmail.com>
2022-03-28 01:14:00 +0100

    Got to the last section of chapter 6

--- enums/target/debug/deps/enums.d
+++
@@ -1,6 +0,0 @@
-C:\Users\blake\Documents\Programming\Rust\the_book\enums\target\debug\deps\enums.exe: src\main.rs src\match_example.rs
-
-C:\Users\blake\Documents\Programming\Rust\the_book\enums\target\debug\deps\enums.d: src\main.rs src\match_example.rs
-
-src\main.rs:
-src\match_example.rs:
--- enums/target/rls/debug/deps/enums-77cf972bd87830ad.d
+++
@@ -1,6 +0,0 @@
-c:\Users\blake\Documents\Programming\Rust\the_book\enums\target\rls\debug\deps\enums-77cf972bd87830ad.rmeta: src\main.rs src\match_example.rs
-
-c:\Users\blake\Documents\Programming\Rust\the_book\enums\target\rls\debug\deps\enums-77cf972bd87830ad.d: src\main.rs src\match_example.rs
-
-src\main.rs:
-src\match_example.rs:
